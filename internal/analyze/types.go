3984176dba1a46f15b7b4a8334135b5d12f7a3ce
mozuku55 <ryo.tako.05@gmail.com>
2026-02-20 16:31:03 +0900

    fix

--- internal/analyze/types.go
+++ internal/analyze/types.go
@@ -1,10 +1,5 @@
 package analyze
 
-type Function struct {
-	Name        string `json:"name"`
-	PackagePath string `json:"package_path"`
-}
-
 type FnDITreeNode struct {
 	Name    string          `json:"name"`
 	PkgPath string          `json:"pkg_path"`
